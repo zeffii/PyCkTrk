2bc2e4d4f3dd42bc205e411ee706d20db2141eb3
zeffii <tetha.z@gmail.com>
2019-07-20 10:19:08 +0200

    correct project mini gpl

--- ptrk_test.py
+++ ptrk_test.py
@@ -1,4 +1,4 @@
-# This file is part of project Sverchok. It's copyrighted by the contributors
+# This file is part of project PyCkTrk. It's copyrighted by the contributors
 # recorded in the version control history of the file, available from
 # its original location https://github.com/zeffii/PyCkTrk/commit/master
 #  
