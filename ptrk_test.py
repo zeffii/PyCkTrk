e46208c5380059ca42c714446c954df3cd024887
zeffii <tetha.z@gmail.com>
2019-07-20 10:16:57 +0200

    new main file

---
+++ ptrk_test.py
@@ -0,0 +1,7 @@
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/zeffii/PyCkTrk/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
