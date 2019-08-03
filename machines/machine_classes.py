d179939db21e999062253bdb50dde45e6f5a50c3
zeffii <tetha.z@gmail.com>
2019-08-03 15:08:32 +0200

    flesh out 2 demo synth schemes

--- machines/machine_classes.py
+++ machines/machine_classes.py
@@ -44,10 +44,63 @@ class Tempo(MachineUtils):
 
 class DrumMK1(MachineUtils):
     def __init__(self):
-        self.scheme = "... .. .. .. .. .. .."
+        self.scheme = {
+            "group": {
+                "name": "params",
+                "description": "drum parameters",
+                "columns": {
+                    #  name    type   length/dots
+                    0: ("thump", "hex", 2),
+                    1: ("volume", "hex", 2),
+                    2: ("attack", "hex", 2),
+                    3: ("decay", "hex", 2),
+                    4: ("sustain", "hex", 2),
+                    5: ("release", "hex", 2),
+                    6: ("adsr speed", "hex", 2),
+                    7: ("click", "bool", 1),
+                }},
+            "track": {
+                "name": "track",
+                "description": "note track",
+                "columns": {
+                    #  name    type   length/dots
+                    0: ("note", "note", 3),
+                    1: ("vol", "hex", 2),
+                    2: ("pan", "hex", 2)
+                }},
+            "max tracks": 1
+        }
 
 
 class Syn1(MachineUtils):
     def __init__(self):
-        self.scheme = "... .. .. .. .. .. .."
+        self.scheme = {
+            "group": {
+                "name": "params",
+                "description": "synth params",
+                "columns": {
+                    #  name    type   length/dots
+                    0: ("osc 1", "hex", 1),
+                    1: ("osc 2", "hex", 1),
+                    2: ("osc 3", "hex", 1),
+                    3: ("mix 1", "hex", 2),
+                    4: ("mix 2", "hex", 2),
+                    5: ("mix 3", "hex", 2),
+                    6: ("attack", "hex", 2),
+                    7: ("decay", "hex", 2),
+                    8: ("sustain", "hex", 2),
+                    9: ("release", "hex", 2),
+                    10: ("adsr speed", "hex", 2),
+                }},
+            "track": {
+                "name": "track",
+                "description": "note track",
+                "columns": {
+                    #  name    type   length/dots
+                    0: ("note", "note", 3),
+                    1: ("vol", "hex", 2),
+                    2: ("pan", "hex", 2)
+                }},
+            "max tracks": 6
+        }
 
