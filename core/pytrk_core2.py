dbe97590007846a84587019f7ba12928e78dbdb1
zeffii <tetha.z@gmail.com>
2019-08-10 13:08:59 +0200

    fill new stub functions/classes

--- core/pytrk_core2.py
+++ core/pytrk_core2.py
@@ -5,10 +5,10 @@
 class Machine:
 
     def __init__(self, machine_type, num_tracks=0):
-        self.internals = make_instance_of(machine_type, num_tracks)
+        self.instance = make_instance_of(machine_type, num_tracks)
         self.pattern_dict = {}
 
-    def add_pattern(self):
+    def add_pattern(self, rows):
         self.pattern_dict
 
 
@@ -16,15 +16,43 @@ class Machine:
 
 class Pattern:
 
-    def __init__(self, machine_reference):
-        self.machine_reference = machine_reference
+    def __init__(self, machine_instance, rows=16):
+        self.machine_instance = machine_instance
+        self.data = {"group": {}, "tracks": {}}
+        self.data['rows'] = rows
 
-    def change_row_count(self):
-        ...
+    def change_row_count(self, new_count):
+        diff = self.data['rows'] - new_count
+        if diff == 0:
+            # do nothing
+            return
+        elif diff < 0:
+            ... # add abs(diff) rows
+
+        elif diff > 0:
+            ... # remove abs(diff) rows
+
+        # change bookkeeping (for now)
+        self.data['rows'] = new_count
 
     def wipe_pattern_data(self):
         ...
 
-    def interpolate_selection(self, row_start, row_end, column_start, column_end):
+    def interpolate_selection(self, row_start, row_end, column_identifier):
         ...
 
+    def transpose_selection(self):
+        ...
+
+    def print_readable(self):
+        columns_in_group = self.machine_instance.columns_in_group
+        columns_in_track = self.machine_instance.columns_in_track
+        num_tracks = len(self.data["tracks"])
+
+        for row in range(self.data['rows']):
+            ...
+
+    def clone(self):
+        new_pattern = Pattern(self.machine_instance, rows=self.data['rows'])
+        new_pattern.data = deepcopy(self.data)
+        return new_pattern
