96bd70b00078a7f95fa92bf5d4470f5d09095b3b
zeffii <tetha.z@gmail.com>
2019-08-11 14:51:11 +0200

    add drumsyn example

--- core/pytrk_core2.py
+++ core/pytrk_core2.py
@@ -24,11 +24,45 @@ class MachinePrototype:
             'track': [k, (v.name, v.dots) for k, v in self.track_params.items()] if hasattr(self, 'track_params') else []}
 
 class GenTempo(MachinePrototype):
+    """
+    This machine represents:
+    - bpm (beats per minute)
+    - tpb (ticks per beat)
+    """
     def __init__(self):
         self.group_params = {
             0: CKProperty(name='bpm', kind='hex', min='000', max='200', default='090'),
             1: CKProperty(name='tpb', kind='hex', min='00', max='20', default='08')
         }
+
+class GenDrumSyn1(MachinePrototype):
+    """
+    A primitive drummachine example
+    """
+    def __init__(self):
+        self.group_params = {
+            0: CKProperty(name='volume', kind='hex', min='000', max='200', default='090'),
+            1: CKProperty(name='thump', kind='hex', min='00', max='20', default='08')
+            2: CKProperty(name='attack', kind='hex', min='00', max='20', default='02')
+            3: CKProperty(name='decay', kind='hex', min='00', max='60', default='28')
+            4: CKProperty(name='sustain', kind='hex', min='00', max='60', default='48')
+            5: CKProperty(name='release', kind='hex', min='00', max='50', default='18')
+            6: CKProperty(name='speed adsr', kind='hex', min='00', max='80', default='10')
+            7: CKProperty(name='click', kind='hex', min='00', max='01', default='01')
+        }
+        self.track_params = {
+            0: CKProperty(name="note", kind="note", default="C-5")
+            1: CKProperty(name="vol", kind="hex", min='00', max='80', default="80")
+            2: CKProperty(name="pan", kind="hex", min='0', max='80', default="40")
+        }
+        self.max_tracks = 1
+        self.source_path = "ck/generators/drumsyn1.ck"
+
+class GenSynth1(MachinePrototype):
+    """
+    """
+    def __init__(self):
+        ...
 
 
 class Machine:
