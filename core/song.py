68730e51a9ff4fbf5fcc85f278d7638459998fb4
zeffii <tetha.z@gmail.com>
2019-08-12 19:17:50 +0200

    add prelim functions to SongFile

--- core/song.py
+++ core/song.py
@@ -8,6 +8,7 @@ class SongFile:
     def __init__(self):
         self.name = "untitled"
         self.machines = set()
+        self.tracks = []
 
     def set_name(self, new_name):
         self.name = new_name
@@ -15,6 +16,11 @@ class SongFile:
     def add_machine(self, machine_type):
         self.machines.add(Machine(machine_type)) 
 
+    def attach_machine_to_track(self, machine, track_index):
+        self.tracks.insert(track_index, machine)
+
+    def remove_machine_from_track(self, machine):
+        self.tracks.pop(... machine)
 
 
 
