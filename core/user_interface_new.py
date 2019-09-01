fffc0f4d7dec63966f29831271018d4339108d6c
zeffii <tetha.z@gmail.com>
2019-09-01 21:25:51 +0200

    tidy up class properties

--- core/user_interface_new.py
+++ core/user_interface_new.py
@@ -4,8 +4,14 @@ class Example(Frame):
 
     def __init__(self):
         super().__init__()
-        self.init_sequencer_ui()
+
         self.caret_cell = [0, 0]
+        self.cell_width = 50
+        self.cell_spacer = 3
+        self.line_height = 15
+        self.line_length = 35
+
+        self.init_sequencer_ui()
 
     def move_caret(self, direction):
 
@@ -18,12 +24,13 @@ class Example(Frame):
 
         self.caret_cell[0] += x
         self.caret_cell[1] += y
-        cell_width = 50
-        cell_spacer = 3
-        line_height = 15
 
-        
-        self.canvas.move(self.caret, x * (cell_width + cell_spacer), y * line_height)
+        #
+        # limit caret cell to x > 0  and y > 0
+        #
+
+        print(self.caret_cell)        
+        self.canvas.move(self.caret, x * (self.cell_width + self.cell_spacer), y * self.line_height)
 
     def init_sequencer_ui(self):
 
@@ -32,44 +39,41 @@ class Example(Frame):
         self.canvas.grid(row=0, column=0)
 
         num_tracks = 6
-        line_length, line_height = 35, 15
-        cell_width = 50
-        cell_spacer = 3
         x_start, y_start = [5, 15]
 
         for i in range(5):
             # draw row divider ( left hand )
-            y_pos = y_start + (line_height * i)
-            self.canvas.create_line(x_start, y_pos, x_start + line_length, y_pos, fill="#999")
+            y_pos = y_start + (self.line_height * i)
+            self.canvas.create_line(x_start, y_pos, x_start + self.line_length, y_pos, fill="#999")
             
             # draw row tick
             self.canvas.create_text(x_start, y_pos, anchor='nw', text=str(int(i * 16)), fill="#333")
             
             # draw individual cells
             for track in range(num_tracks):
-                rc_x1 = x_start + line_length + (track * cell_width) + (cell_spacer * track)
+                rc_x1 = x_start + self.line_length + (track * self.cell_width) + (self.cell_spacer * track)
                 rc_y1 = y_pos
-                rc_x2 = rc_x1 + cell_width
-                rc_y2 = rc_y1 + line_height
+                rc_x2 = rc_x1 + self.cell_width
+                rc_y2 = rc_y1 + self.line_height
                 self.canvas.create_rectangle(rc_x1, rc_y1, rc_x2, rc_y2, outline="#bbb", fill="#ddd")
 
         # draw column divs
         for track in range(num_tracks):
-            rc_x1 = x_start + line_length + (track * cell_width) + (cell_spacer * track)
+            rc_x1 = x_start + self.line_length + (track * self.cell_width) + (self.cell_spacer * track)
             self.canvas.create_line(rc_x1 - 1, 0, rc_x1 - 1 , 200, fill="#888")
 
         # draw column track names
         for track in range(num_tracks):
-            rc_x1 = x_start + line_length + (track * cell_width) + (cell_spacer * track)
-            self.canvas.create_text(rc_x1, 0, text="trk:"+str(track), anchor="nw", fill="#222")
+            rc_x1 = x_start + self.line_length + (track * self.cell_width) + (self.cell_spacer * track)
+            self.canvas.create_text(rc_x1, 0, text="trk:" + str(track), anchor="nw", fill="#222")
 
         # draw sequencer caret
         track = 0
         y_pos = y_start
-        rc_x1 = x_start + line_length
+        rc_x1 = x_start + self.line_length
         rc_y1 = y_pos
-        rc_x2 = rc_x1 + cell_width
-        rc_y2 = rc_y1 + line_height
+        rc_x2 = rc_x1 + self.cell_width
+        rc_y2 = rc_y1 + self.line_height
         self.caret = self.canvas.create_rectangle(rc_x1, rc_y1, rc_x2, rc_y2, outline="#bbb", fill="#36a")
 
     def key_pressed(self, event):
