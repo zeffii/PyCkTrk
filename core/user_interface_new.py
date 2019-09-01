b755957c2b85fdfec42eee4a6ea2f51199a0dbfa
zeffii <tetha.z@gmail.com>
2019-09-01 16:02:25 +0200

    implement rudimentary caret movement with canvas.bind

--- core/user_interface_new.py
+++ core/user_interface_new.py
@@ -5,11 +5,16 @@ class Example(Frame):
     def __init__(self):
         super().__init__()
         self.init_sequencer_ui()
+        self.caret_cell = [0, 0]
+
+    def move_caret(self, direction):
+        self.canvas.move(self.caret, 20.0, 0.0)
 
     def init_sequencer_ui(self):
+
         self.pack(fill=BOTH, expand=1)
-        canvas = Canvas(self, width=400, height=400, background="#aaa")
-        canvas.grid(row=0, column=0)
+        self.canvas = Canvas(self, width=400, height=400, background="#aaa")
+        self.canvas.grid(row=0, column=0)
 
         num_tracks = 6
         line_length, line_height = 35, 15
@@ -20,10 +25,10 @@ class Example(Frame):
         for i in range(5):
             # draw row divider ( left hand )
             y_pos = y_start + (line_height * i)
-            canvas.create_line(x_start, y_pos, x_start + line_length, y_pos, fill="#999")
+            self.canvas.create_line(x_start, y_pos, x_start + line_length, y_pos, fill="#999")
             
             # draw row tick
-            canvas.create_text(x_start, y_pos, anchor='nw', text=str(int(i * 16)), fill="#333")
+            self.canvas.create_text(x_start, y_pos, anchor='nw', text=str(int(i * 16)), fill="#333")
             
             # draw individual cells
             for track in range(num_tracks):
@@ -31,26 +36,40 @@ class Example(Frame):
                 rc_y1 = y_pos
                 rc_x2 = rc_x1 + cell_width
                 rc_y2 = rc_y1 + line_height
-                canvas.create_rectangle(rc_x1, rc_y1, rc_x2, rc_y2, outline="#bbb", fill="#ddd")
+                self.canvas.create_rectangle(rc_x1, rc_y1, rc_x2, rc_y2, outline="#bbb", fill="#ddd")
 
         # draw column divs
         for track in range(num_tracks):
             rc_x1 = x_start + line_length + (track * cell_width) + (cell_spacer * track)
-            canvas.create_line(rc_x1 - 1, 0, rc_x1 - 1 , 200, fill="#888")
+            self.canvas.create_line(rc_x1 - 1, 0, rc_x1 - 1 , 200, fill="#888")
 
         # draw column track names
         for track in range(num_tracks):
             rc_x1 = x_start + line_length + (track * cell_width) + (cell_spacer * track)
-            canvas.create_text(rc_x1, 0, text="trk:"+str(track), anchor="nw", fill="#222")
+            self.canvas.create_text(rc_x1, 0, text="trk:"+str(track), anchor="nw", fill="#222")
 
-        print('here')
+        # draw sequencer caret
+        track = 0
+        y_pos = y_start
+        rc_x1 = x_start + line_length
+        rc_y1 = y_pos
+        rc_x2 = rc_x1 + cell_width
+        rc_y2 = rc_y1 + line_height
+        self.caret = self.canvas.create_rectangle(rc_x1, rc_y1, rc_x2, rc_y2, outline="#bbb", fill="#36a")
+
+    def key_pressed(self, event):
+        self.move_caret(event.char)
+
 
 
 def main():
 
+
     root = Tk()
     ex = Example()
+    ex.focus_set()
     root.geometry('750x600')
+    ex.bind("<Key>", ex.key_pressed)
     root.title("PyCkTrk! v.0001")
     root.mainloop()
 
